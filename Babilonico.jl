166d65d0f29ca669a1a6605654efa797ada82747
Kevin Rivera <kevinrc.unam@gmail.com>
2021-08-03 20:00:34 -0500

    metodo babilonico en julia

---
+++ Babilonico.jl
@@ -0,0 +1,19 @@
+function babilonico(y)
+    aprox = 1
+    x = aprox
+    e = 1
+    while e > 1e-3
+        xn = x
+        x = 1/2*(x+(y/x))
+        e = abs(x-xn)
+        print("xn+1 = $(x) \nerror = $(e)\n\n")
+    end
+    return x
+end
+
+y = 55
+x = babilonico(y)
+print("\nLa raiz cuadrada de $(y) es $(x)\n")
+
+
+
