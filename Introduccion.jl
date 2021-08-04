1efaa64319b1bcb4e52be2cd032056937ee887d7
Kevin Rivera <kevinrc.unam@gmail.com>
2021-08-03 21:43:03 -0500

    Introduccion a Julia

---
+++ Introduccion.jl
@@ -0,0 +1,102 @@
+println("Hola mundo")
+
+3 + 2 
+
+3 - 1; #Ejecucion silenciosa
+
+div(10,2)
+
+? pwd
+
+pwd()
+
+; ls #; ejecutar comandos
+
+x = 3
+
+α = 5 #\alpha *Tab*
+
+β = 6 #\beta *Tab*
+
+name = "Kevin"
+greeting = "Hello, $name"
+print(greeting)
+
+Int8 #Int *Tab* (Muestra tipos de Int)
+
+a = Int8(1)
+b = Int8(2)
+a+b
+
+typeof(ans)
+
+a = Int(1e5) #Notacion exponencial
+
+typeof(a)
+
+a = 5
+
+typeof(a)
+
+supertype(Signed)
+
+supertype(Integer)
+
+subtypes(Real)
+
+subtypes(Integer)
+
+subtypes(Signed)
+
+@show isconcretetype(Float64);
+
+@show isconcretetype(Real);
+
+function _show_subtype_tree()
+
+big(10)
+
+typeof(ans)
+
+10^5
+
+10 ^10
+
+10 ^ 19 #Error
+
+bten = big(10)
+
+bten ^ 19
+
+typeof(ans)
+
+typemax(Int64)
+
+typemin(Int64)
+
+a = 7
+c = (1+3.5im) * a
+
+typeof(c)
+
+c.re, c.im
+
+conj(c)
+
+c
+
+3//4
+
+typeof(ans)
+
+3//4 + 5//6
+
+s1 = "Esto es una String"
+
+s2 = """Esto es un String"""
+
+typeof('a')
+
+typeof("a")
+
+
